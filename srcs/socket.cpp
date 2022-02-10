0a35271bbff85e215a765cba9ef417f57a0e4d00
Florian HAMEL <fhamel@e1r10p21.42.fr>
2022-02-10 20:04:05 +0100

    pipe_data

--- srcs/socket.cpp
+++ srcs/socket.cpp
@@ -6,61 +6,11 @@
 /*   By: fhamel <fhamel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 19:04:05 by fhamel            #+#    #+#             */
-/*   Updated: 2022/02/07 15:36:07 by fhamel           ###   ########.fr       */
+/*   Updated: 2022/02/10 18:16:57 by fhamel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "socket.hpp"
-
-/*
-** If an error occurs before the server is actually started,
-** all the socket fds are closed before exit
-*/
-void	closeListen(std::vector<int> sockListen, size_t endInd)
-{
-	for (size_t i = 0; i < endInd; ++i) {
-		close(sockListen[i]);
-	}
-}
-
-/*
-** A socket can only listen on one port at a time
-** so we bind a socket with every ports specified
-*/
-std::vector<int>	sockListen(std::vector<size_t>	ports)
-{
-	std::vector<int>	sockArr;
-	int					sockTmp;
-	sockaddr_in			addr;
-	for (size_t i = 0; i < ports.size(); ++i) {
-		if ((sockTmp = socket(AF_INET, SOCK_STREAM, 0)) == ERROR) {
-			perror("socket");
-			closeListen(sockArr, i);
-			exit(EXIT_FAILURE);
-		}
-		int opt = 1;
-		if (setsockopt(sockTmp, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == ERROR) {
-			perror("setsockopt");
-			closeListen(sockArr, i);
-			exit(EXIT_FAILURE);
-		}
-		addr.sin_family = AF_INET;
-		addr.sin_addr.s_addr = INADDR_ANY;
-		addr.sin_port = htons(ports[i]);
-		if (bind(sockTmp, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) == ERROR) {
-			perror("bind");
-			closeListen(sockArr, i);
-			exit(EXIT_FAILURE);
-		}
-		if (listen(sockTmp, 1024) == ERROR) {
-			perror("listen");
-			closeListen(sockArr, i);
-			exit(EXIT_FAILURE);
-		}
-		sockArr.push_back(sockTmp);
-	}
-	return sockArr;
-}
 
 /*
 ** Sockets starting function
@@ -69,7 +19,7 @@ void	sockServ(std::vector<Server> servers, std::vector<size_t> ports)
 {
 	SockData	sockData;
 	sockData.setServers(servers);
-	sockData.setSockListen(sockListen(ports));
+	sockData.setSockListen(ports);
 	for (size_t i = 0; i < sockData.getSizeListen(); ++i) {
 		sockData.addActiveSet(sockData.getSockListen(i));
 	}
