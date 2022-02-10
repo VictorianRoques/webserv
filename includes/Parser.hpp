f907bb62aac8dcb521cce7baef7751b465eebcbb
Padoue NIELLY <pnielly@student.42.fr>
2022-02-10 17:54:10 +0100

    added exception pathIsFile(errorPage)

--- includes/Parser.hpp
+++ includes/Parser.hpp
@@ -6,7 +6,7 @@
 /*   By: pnielly <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 19:16:26 by pnielly           #+#    #+#             */
-/*   Updated: 2022/02/09 11:03:41 by pnielly          ###   ########.fr       */
+/*   Updated: 2022/02/10 17:49:40 by pnielly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,6 +91,7 @@ class Parser: public Server {
 		void	serverChecker();
 		bool	serverNameChecker();
 		bool	locationChecker();
+		bool	errorPageChecker();
 
 		//EXCEPTION
 
@@ -121,6 +122,8 @@ class Parser: public Server {
 		class EmptyConfigException: public std::exception { public: virtual char const *what() const throw(); };
 
 		class MissingDefaultLocationException: public std::exception { public: virtual char const *what() const throw(); };
+		
+		class ErrorPagePathException: public std::exception { public: virtual char const *what() const throw(); };
 
 };
 
