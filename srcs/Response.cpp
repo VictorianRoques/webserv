4a6e9fe6b21f21ad6e798b91cd95a9748bad3618
Victorian ROQUES <viroques@e1r9p18.42.fr>
2022-02-16 18:48:26 +0100

    add -2

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: viroques <viroques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 18:33:19 by viroques          #+#    #+#             */
-/*   Updated: 2022/02/16 15:19:53 by viroques         ###   ########.fr       */
+/*   Updated: 2022/02/16 17:47:51 by viroques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,9 @@ int		Response::searchFd(Request &req)
 	if (initRequest(req))
 		return _fd;
 	if (needCgi())
+	{
 		return (-1);
+	}
 	answer();
 	return _fd;
 }
